89b7a64ee66fc63483781ff83a46776bd0a1928b
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-09-04 10:13:47 +0400

    Adding update board functionality

--- bin/main.rb
+++ bin/main.rb
@@ -1,24 +1,80 @@
 #!/usr/bin/env ruby
 puts 'Welcome to Tic Tac Toe game'
 puts
-
-sleep 1
-
+puts
 # create validate method to check if valid name is typed
+
+@board = [
+  [{ '1': nil }, { '2': nil }, { '3': nil }],
+  [{ '4': nil }, { '5': nil }, { '6': nil }],
+  [{ '7': nil }, { '8': nil }, { '9': nil }]
+]
+
+def show_board
+  puts '+---------------------------+'
+
+  @board.each do |arr|
+    print '|'
+    arr.each do |el|
+      el.each do |key, value|
+        if value
+          print " #{key} - #{value == 'x' ? "\u{274C}" : "\u2B55"}  "
+        else
+          print " #{key} - \u{1F4A9}  "
+        end
+      end
+    end
+    print '|'
+    puts
+  end
+
+  puts '+---------------------------+'
+end
+
+# ----------------  Validation part ------------------
+
+def validate_name(name)
+  while name.length < 3
+    p 'name is too short,try again'
+    name = gets.chomp
+  end
+
+  name
+end
+
+def validate_move(num)
+  while num.to_i < 1 || num.to_i > 9
+    p 'not a valid number, please choose a number between 1 to 9'
+    num = gets.chomp
+  end
+  num
+end
+
+# starting from here
 
 puts 'Player 1 type your name'
 
-@player_1_name = gets.chomp
+@player_1_name = validate_name(gets.chomp)
 puts
 
 puts 'Player 2 add your name'
 
-@player_2_name = gets.chomp
+@player_2_name = validate_name(gets.chomp)
 puts
 
-puts 'Computer will randomly choose sign for players'
-puts '...'
-sleep 1
+puts 'Computer will randomly choose sign for players, please wait ... '
+
+def show_progress
+  101.times do |i|
+    print("\rPercentage: ", "#{i}%")
+    sleep(0.03)
+  end
+end
+
+show_progress
+puts
+puts
+puts
 
 # make module for methods and move generate_random_signs there
 
@@ -42,48 +98,13 @@ end
   player_sign: @player_2_sign
 }
 
-puts "#{@player1[:player_name]}(#{@player1[:player_sign]}) --vs-- #{@player2[:player_name]}(#{@player2[:player_sign]})"
+puts "#{@player1[:player_name]}(#{@player1[:player_sign]}) -- vs -- #{@player2[:player_name]}(#{@player2[:player_sign]})"
 puts
 sleep 1
 
-def check_first_player(player_1_sign, player_2_sign)
-  first_player = player_1_sign == 'x' ? @player_1_name : @player_2_name
-  second_player = player_2_sign == 'o' ? @player_1_name : @player_2_name
-  [first_player, second_player]
-end
-
-toggler = 0
-current_player = check_first_player(@player_1_sign, @player_2_sign)[toggler]
-
 # board
-@board = [
-  [{ '1': 'x' }, { '2': nil }, { '3': nil }],
-  [{ '4': '0' }, { '5': 'o' }, { '6': 'x' }],
-  [{ '7': 'x' }, { '8': 'o' }, { '9': 'x' }]
-]
+show_board
 
-# p ([@board[0], @board[1], @board[2]].flatten.find { |el| el.include? :'9' })
-
-def show_board
-  puts '+---------------------------+'
-
-  @board.each do |arr|
-    print '|'
-    arr.each do |el|
-      el.each do |key, value|
-        if value
-          print " #{key} - #{value == 'x' ? "\u{274C}" : "\u2B55"}  "
-        else
-          print " #{key} - \u{1F4A9}  "
-        end
-      end
-    end
-    print '|'
-    puts
-  end
-
-  puts '+---------------------------+'
-end
 
 @wining_combinations = [
   [1, 2, 3], [4, 5, 6], [7, 8, 9],
@@ -91,18 +112,26 @@ end
   [1, 5, 9], [3, 5, 7]
 ]
 
-def error_message
-  'Something went wrong . Please choose number from 1 to 9'
+# getting number and sign to update the board
+def update_board(num, sign)
+  @board = @board.map do |row|
+    row.each do |item|
+      item.each do |key, _value|
+        item[key] = sign if key == :"#{num}"
+      end
+    end
+  end
+  puts
+  puts
+  show_board
+  puts
+  puts
 end
 
-def validate_move(number)
-  p error_message if number < 1 || number > 9
-end
-
-def make_move(_player, num)
-  validate_move(num)
-  p num
-  # replace num inside border with players sign
+def make_move(cur_player)
+  puts "#{cur_player[:player_name]} it's your turn, please choose a number between 1 to 9"
+  num = validate_move(gets.chomp)
+  update_board(num, cur_player[:player_sign])
 end
 
 def get_winner_name(combinations, board, player)
@@ -131,15 +160,26 @@ def winner?
   winner_name
 end
 
-p winner?
+def check_players_order
+  if @player1[:player_sign] == 'x'
+    first_player = @player1
+    second_player = @player2
+  else
+    first_player = @player2
+    second_player = @player1
+  end
 
-# 9.times do
-#   current_player = check_first_player(player_1_sign, player_2_sign)[toggler]
-#   # show board
-#   puts "#{current_player} your turn,type number from 1 to 9"
-#   board_number = gets.chomp.to_i
-#   make_move(current_player, board_number)
-#   # update_board  create method update board
-#   # check if there is winner then break loop
-#   toggler = toggler.zero? ? 1 : 0
-# end
+  [first_player, second_player]
+end
+
+@toggler = 0
+
+until winner?
+
+  current_player = check_players_order[@toggler]
+  make_move(current_player)
+  @toggler = @toggler.zero? ? 1 : 0
+
+end
+
+
