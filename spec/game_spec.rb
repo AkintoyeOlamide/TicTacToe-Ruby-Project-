55b7e956f0662d36b2a035b15779978e65bb97f0
GraceOyiza <graceoyiza13@gmail.com>
2020-09-17 13:13:46 +0100

    Add testing for validate_move method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,25 +6,24 @@ RSpec.describe "testing the game methods" do
      let(:player2) { Player.new('Olamide', 'o')} 
      let(:game) { Game.new(player1, player2)} 
 
-    #  it "it chooses sign for players" do
-    #     arr =  Game.choose_signs
-    #     expect(arr.sort).to eq(['x', 'o'].sort)
+     it "it chooses sign for players" do
+        arr =  Game.choose_signs
+        expect(arr.sort).to eq(['x', 'o'].sort)
          
-    #  end
-
-    #  context 'validate the players move' do     
-    #  it "it should check the moves between 1 to 9" do
-    #     expect(game.validate_move(6)).to eq(6)
-         
-    #  end
-    # end
-
-     it 'get the current player' do
-        name = "grace"
-        expect(name.getcurrent_player).to eq('grace')
      end
-
     
+
+     context 'validate the players move' do     
+         it "it should check the moves between 1 to 9" do
+            expect(game.validate_move(6)).to eq(6)   
+         end
+
+         it "it should check if number is invalid" do
+            expect(game.validate_move('p')).to eq(-2)   
+         end
+      end
+
+   
      
     
 end
